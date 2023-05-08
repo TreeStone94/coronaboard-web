efbd40fed35c5c9cc508fdeeac22597afeae9900
남우석 <nws94@douzone.com>
2023-05-09 00:06:05 +0900

    bootstrap 예제

---
+++ src/pages/bootstrap/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const BootstrapPage = () => {
+  return (
+    <div>코로나 보드!</div>
+  )
+}
+
+export default BootstrapPage;
---
+++ src/pages/bootstrap/select.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap"
+import Select from 'react-select';
+
+const options = [
+    {value: 'KR', label: '한국'},
+    {value: 'JP', label: '일본'},
+    {value: 'US', label: '미국'},
+    {value: 'CN', label: '중국'},
+]
+
+const SelectPage = () => {
+  const [selectedOptionSingle, setSelectedOptionSingle] = useState();
+  const [selectedOptionMulti, setSelectedOptionMulti] = useState();
+
+  return (
+    <Container className={'pt-3'}>
+      <h5>단일 선택 상자</h5>
+      <Select
+        value={selectedOptionSingle}
+        onChange={(selectedOption) => {
+          console.log('selectedOptionSingle', selectedOption);
+          setSelectedOptionSingle(selectedOption);
+        }}
+        options={options}/>
+
+      <hr/>
+      <h5>다중 선택 상자</h5>
+      <Select
+        isMulti={true}
+        isSearchable={true}
+        placeholder={'국가 선택...'}
+        value={selectedOptionMulti}
+         onChange={(selectedOption) => {
+          console.log('selectedOptionMulti', selectedOption);
+          setSelectedOptionMulti(selectedOption);
+        }}
+        options={options}
+      />
+    </Container>
+  )
+}
+
+export default SelectPage
